cf1ee863d9a61c7410b6a3f4c481f039274c62f7
Vigneswaran321 <smartben0431@gmail.com>
2024-03-02 18:46:13 +0530

    AI Stream

--- app.py
+++ app.py
@@ -3,27 +3,37 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
-latest_headlines = [{"text": "", "time": None}] * 10  # Initialize with empty headlines
+latest_headlines = [{"text": "", "time": None}] * \
+    10  # Initialize with empty headlines
+
 
 def get_latest_headlines():
     url = 'https://www.prnewswire.com/news-releases/news-releases-list/'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     headlines = soup.find_all('h3')
-    return [headline.text.strip() for headline in headlines[:10]]  # Get the latest 10 headlines
+    # Get the latest 10 headlines
+    return [headline.text.strip() for headline in headlines[:10]]
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+
 @app.route('/get_latest_headlines')
 def send_latest_headlines():
     headlines = get_latest_headlines()
     print('Latest headlines:', headlines)  # Add print statement to debug
     return jsonify({"headlines": headlines})
+
 
 if __name__ == '__main__':
     app.run(debug=False)
